37e14cf2e83dc8ee9268d405b18538a7fb676e9b
Scott Keplinger <skeplin17@gmail.com>
2020-03-19 01:51:06 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,27 @@
+function forLoop(array) {
+  for (let i = 0; i < 25; i++)
+  if (i === 1) {
+   array.push ("I am 1 strange loop.")
+ } else {
+   array.push (`I am ${i} strange loops.`)
+ }
+ return array
+}
+
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n)
+    n -= 1
+  }
+  return "done"
+}
+
+function doWhileLoop(num) {
+  function incrementVariable() {
+    num = num + 1;
+    return num;
+}
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+}
